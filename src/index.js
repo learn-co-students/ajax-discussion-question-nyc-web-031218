ee3d1a993b1a22e145d48d2011dbbf3a34180f61
Eric Kim <hyeokjungkim@gmail.com>
2018-04-30 10:02:53 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+  const button = document.getElementsByClassName('btn')[0]
+  console.log(button)
+  button.addEventListener("click", () => {
+    var data = fetch('https://randomuser.me/api/')
+    .then( res => res.json() )
+    .then( console.log )
+  })
 });
